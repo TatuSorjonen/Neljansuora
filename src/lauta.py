185737c7cf27779d26f40e9ab436609e48090cea
Tatu Sorjonen <tatu.sorjonen@gmail.com>
2023-09-15 12:32:59 +0300

    Viikko2

--- src/lauta.py
+++ src/lauta.py
@@ -9,27 +9,49 @@ class Tulos(Enum):
     
 class Lauta():
 
+    SARAKKEIDEN_MAARA = 7 
+    RIVIEN_MAARA = 6
+    KELTAINEN = 'K'
+    PUNAINEN = 'P'
+    TYHJA = '-'
+    KELTAISEN_VUORO = 1
+    PUNAISEN_VUORO = 2 
+
     def __init__(self):
-        self.SARAKKEIDEN_MAARA = 7 
-        self.RIVIEN_MAARA = 6
-        self.KELTAINEN = 'K'
-        self.PUNAINEN = 'P'
-        self.TYHJA = '-'
-        self.KELTAISEN_VUORO = 1
-        self.PUNAISEN_VUORO = 2 
         self.tulos = Tulos.MENEILLAAN
-        self.lauta = [['-' for i in range(self.SARAKKEIDEN_MAARA)] for j in range(self.RIVIEN_MAARA)]
-        self.kenen_vuoro = self.KELTAISEN_VUORO
+        self.ruudukko = [['-' for i in range(Lauta.SARAKKEIDEN_MAARA)] for j in range(Lauta.RIVIEN_MAARA)]
+        self.kenen_vuoro = Lauta.KELTAISEN_VUORO
         
-    def lisaa_keltainen(self, y, x):
-        if not self.onko_varattu(y, x):
-            self.lauta[y][x] = self.KELTAINEN
-            self.kenen_vuoro = self.PUNAISEN_VUORO
+    def lisaa_keltainen(self, sarake):
+        #print("Lisätään keltainen")
+        vapaa_rivi = self.vapaa_rivi_sarakkeessa(sarake)
+        print(f'Vapaa rivi {vapaa_rivi}')
+        if vapaa_rivi != -1:
+            self.ruudukko[vapaa_rivi][sarake] = Lauta.KELTAINEN
+            self.kenen_vuoro = Lauta.PUNAISEN_VUORO
             
-    def lisaa_sininen(self, y, x):
-        if not self.onko_varattu(y, x):
-            self.lauta[y][x] = self.PUNAINEN
-            self.kenen_vuoro = self.KELTAISEN_VUORO
+    def lisaa_punainen(self, sarake):
+        #print("Lisätään punainen")
+        vapaa_rivi = self.vapaa_rivi_sarakkeessa(sarake)
+        print(f'Vapaa rivi {vapaa_rivi}')
+        if vapaa_rivi != -1:
+            self.ruudukko[vapaa_rivi][sarake] = Lauta.PUNAINEN
+            self.kenen_vuoro = Lauta.KELTAISEN_VUORO
         
-    def onko_varattu(self, y, x):
-        return self.lauta[y][x] != self.TYHJA
+    def vapaa_rivi_sarakkeessa(self, sarake):
+        print(f'{sarake} = sarake')
+        vapaa_rivi_indeksi = -1
+        for rivi in range(Lauta.RIVIEN_MAARA - 1, -1, -1):
+            #print(self.ruudukko[rivi][sarake])
+            print(f'Rivi={rivi}')
+            if self.ruudukko[rivi][sarake] == Lauta.TYHJA:
+                vapaa_rivi_indeksi = rivi
+                return vapaa_rivi_indeksi
+        
+        return vapaa_rivi_indeksi
+        
+    def tulosta_lauta(self):
+        for i in range(len(self.ruudukko)):
+            for j in range(len(self.ruudukko[i])):
+                print(self.ruudukko[i][j], end=' ')
+            print()
--- src/testi.py
+++ src/testi.py
@@ -1,25 +1,28 @@
 import pygame
+import os
+import math
 from lauta import Lauta, Tulos
-
+    
 
 class Neljansuora:
 
+    RUUDUN_KOKO = 150
+    IKKUNAN_LEVEYS = RUUDUN_KOKO * Lauta.SARAKKEIDEN_MAARA
+    IKKUNAN_KORKEUS = RUUDUN_KOKO * Lauta.RIVIEN_MAARA
+    POHJAVARI = (255,250,240)
+    VASTAVARI = (28,134,238)
+    VIIVAN_LEVEYS = 5
+        
     def __init__(self):
         pygame.init()
         self.lauta = Lauta()
-        self.RUUDUN_KOKO = 150
-        self.IKKUNAN_LEVEYS = self.RUUDUN_KOKO * self.lauta.SARAKKEIDEN_MAARA
-        self.IKKUNAN_KORKEUS = self.RUUDUN_KOKO * self.lauta.RIVIEN_MAARA
-        self.POHJAVARI = (200, 200, 200)
-        self.VASTAVARI = (255, 50, 25)
-        self.KELTAINEN = pygame.transform.scale(pygame.image.load\
-        ("src/kuvat/keltainen.jpeg"), (self.RUUDUN_KOKO, self.RUUDUN_KOKO))
-        self.PUNAINEN = pygame.transform.scale(pygame.image.load\
-        ("src/kuvat/punainen.jpeg"), (self.RUUDUN_KOKO, self.RUUDUN_KOKO))
-        self.VIIVAN_LEVEYS = 5
+        self.KELTAINEN_NAPPI = pygame.transform.scale(pygame.image.load\
+        ("src/kuvat/keltainen.jpeg"), (Neljansuora.RUUDUN_KOKO, Neljansuora.RUUDUN_KOKO))
+        self.PUNAINEN_NAPPI = pygame.transform.scale(pygame.image.load\
+        ("src/kuvat/punainen.jpeg"), (Neljansuora.RUUDUN_KOKO, Neljansuora.RUUDUN_KOKO))
         
-        self.ikkuna = pygame.display.set_mode((self.IKKUNAN_LEVEYS, self.IKKUNAN_KORKEUS))
-        self.ikkuna.fill(self.POHJAVARI)
+        self.ikkuna = pygame.display.set_mode((Neljansuora.IKKUNAN_LEVEYS, Neljansuora.IKKUNAN_KORKEUS))
+        self.ikkuna.fill(Neljansuora.POHJAVARI)
 
         
 #rivi = 6
@@ -35,18 +38,43 @@ class Neljansuora:
                 if tapahtuma.type == pygame.QUIT:
                     pygame.quit()
                 elif tapahtuma.type == pygame.MOUSEBUTTONDOWN:
-                    self.piirra_merkki()
+                    self.aseta_merkki(tapahtuma.pos[0], tapahtuma.pos[1])
     
             self.piirra_taulukko()
             pygame.display.update()
         
     
     def piirra_taulukko(self):
-        for x in range(0, self.IKKUNAN_LEVEYS, self.RUUDUN_KOKO):
-            for y in range(0, self.IKKUNAN_KORKEUS, self.RUUDUN_KOKO):
-                ruutu = pygame.Rect(x, y, self.IKKUNAN_LEVEYS, self.IKKUNAN_KORKEUS)
-                pygame.draw.rect(self.ikkuna, self.VASTAVARI, ruutu, self.VIIVAN_LEVEYS)
+        for x in range(0, Neljansuora.IKKUNAN_LEVEYS, Neljansuora.RUUDUN_KOKO):
+            for y in range(0, Neljansuora.IKKUNAN_KORKEUS, Neljansuora.RUUDUN_KOKO):
+                ruutu = pygame.Rect(x, y, Neljansuora.IKKUNAN_LEVEYS, Neljansuora.IKKUNAN_KORKEUS)
+                pygame.draw.rect(self.ikkuna, Neljansuora.VASTAVARI, ruutu, Neljansuora.VIIVAN_LEVEYS)
+                
+    def aseta_merkki(self, hiiri_x, hiiri_y):
+        sarake = math.floor(hiiri_x / Neljansuora.RUUDUN_KOKO)
+        #rivi = math.floor(hiiri_y / Neljansuora.RUUDUN_KOKO)
         
-    def piirra_merkki(self):
-        #Tee piirtää merkin Lauta luokan taulukosta
-        self.ikkuna.blit(self.PUNAINEN, (0, 0))
+        #debug
+        print(f"sarake:{sarake}")
+        print(f"Kenenvuoro:{self.lauta.kenen_vuoro}") 
+        
+        if self.lauta.kenen_vuoro == Lauta.KELTAISEN_VUORO:
+            self.lauta.tulosta_lauta()
+            self.lauta.lisaa_keltainen(sarake)
+        elif self.lauta.kenen_vuoro == Lauta.PUNAISEN_VUORO:
+            self.lauta.tulosta_lauta() #debug muuta
+            self.lauta.lisaa_punainen(sarake)
+        self.piirra_tilanne()
+        
+    def piirra_tilanne(self):
+        self.piirra_taulukko()
+        
+        for x_ruutu in range(0, self.lauta.SARAKKEIDEN_MAARA):
+            for y_ruutu in range(0, self.lauta.RIVIEN_MAARA):
+                x_koordinaatti = Neljansuora.RUUDUN_KOKO * x_ruutu
+                y_koordinaatti = Neljansuora.RUUDUN_KOKO * y_ruutu
+                if self.lauta.ruudukko[y_ruutu][x_ruutu] == 'K':
+                    self.ikkuna.blit(self.KELTAINEN_NAPPI, (x_koordinaatti, y_koordinaatti))
+                elif self.lauta.ruudukko[y_ruutu][x_ruutu] == 'P':
+                    self.ikkuna.blit(self.PUNAINEN_NAPPI, (x_koordinaatti, y_koordinaatti))
+        
